664b04bc19c2725b01f989f425257c3b056f75fb
zhouhuafei <zhouhuafei@shopex.cn>
2018-04-11 20:08:55 +0800

    dev

--- src/index.js
+++ src/index.js
@@ -14,28 +14,26 @@ function timeCountDown(json) {
         },
     }, json);
     let seconds = opts.seconds; // 秒数
-    const allSeconds = seconds; // 总秒数
-    const run = opts.callback.run; // 运行的回调
-    const over = opts.callback.over; // 结束的回调
-
-    if (seconds >= 0) { // 时间大于等于0秒
-        if (opts.isToTime) {
-            run(secondsToTime(seconds)); // 运行时的回调
-        } else {
-            run({day: 0, hours: 0, minutes: 0, seconds: seconds, allSeconds: allSeconds}); // 运行时的回调
-        }
+    if (seconds > 0) { // 时间大于0秒
+        const allSeconds = seconds; // 总秒数
+        const run = opts.callback.run; // 运行的回调
+        const over = opts.callback.over; // 结束的回调
+        const runFn = function () { // 对运行的回调进行二次封装
+            if (opts.isToTime) {
+                run(secondsToTime(seconds)); // 运行时的回调
+            } else {
+                run({day: 0, hours: 0, minutes: 0, seconds: seconds, allSeconds: allSeconds}); // 运行时的回调
+            }
+        };
+        runFn(); // 运行时的回调
         // 倒计时走你
         const timer = setInterval(function () {
             seconds--;
-            if (seconds >= 0) {
-                if (opts.isToTime) {
-                    run(secondsToTime(seconds)); // 运行时的回调
-                } else {
-                    run({day: 0, hours: 0, minutes: 0, seconds: seconds, allSeconds: allSeconds}); // 运行时的回调
-                }
-            } else {
+            if (seconds < 0) {
+                clearInterval(timer); // 清除定时器
                 over(); // 结束时的回调
-                clearInterval(timer);
+            } else {
+                runFn(); // 运行时的回调
             }
         }, 1000);
     } else { // 时间小于0秒
