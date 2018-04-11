931890cacedbbe8b3e4a2d0ffd60197c6138f0e8
zhouhuafei <zhouhuafei@shopex.cn>
2018-04-12 00:54:10 +0800

    md

--- dist/index.js
+++ dist/index.js
@@ -8,25 +8,31 @@ function timeCountDown(json) {
     var opts = extend({
         seconds: 0,
         isToTime: true, // 是否转换成时间
+        isHandleRunWhenZero: false, // 是否运行run回调，当传入的秒数为0
         callback: {
             run: function run() {},
             over: function over() {}
         }
     }, json);
     var seconds = opts.seconds; // 秒数
+    var allSeconds = seconds; // 总秒数
+    var run = opts.callback.run; // 运行的回调
+    var over = opts.callback.over; // 结束的回调
+    var runFn = function runFn() {
+        // 对运行的回调进行二次封装
+        if (opts.isToTime) {
+            run(secondsToTime(seconds)); // 运行时的回调
+        } else {
+            run({ day: 0, hours: 0, minutes: 0, seconds: seconds, allSeconds: allSeconds }); // 运行时的回调
+        }
+    };
+    if (Number(seconds) === 0) {
+        if (opts.isHandleRunWhenZero) {
+            runFn(); // 运行时的回调
+        }
+    }
     if (seconds > 0) {
         // 时间大于0秒
-        var allSeconds = seconds; // 总秒数
-        var run = opts.callback.run; // 运行的回调
-        var over = opts.callback.over; // 结束的回调
-        var runFn = function runFn() {
-            // 对运行的回调进行二次封装
-            if (opts.isToTime) {
-                run(secondsToTime(seconds)); // 运行时的回调
-            } else {
-                run({ day: 0, hours: 0, minutes: 0, seconds: seconds, allSeconds: allSeconds }); // 运行时的回调
-            }
-        };
         runFn(); // 运行时的回调
         // 倒计时走你
         var timer = setInterval(function () {
--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ function timeCountDown(json) {
     const opts = extend({
         seconds: 0,
         isToTime: true, // 是否转换成时间
+        isHandleRunWhenZero: false, // 是否运行run回调，当传入的秒数为0
         callback: {
             run: function () {
             },
@@ -14,17 +15,22 @@ function timeCountDown(json) {
         },
     }, json);
     let seconds = opts.seconds; // 秒数
+    const allSeconds = seconds; // 总秒数
+    const run = opts.callback.run; // 运行的回调
+    const over = opts.callback.over; // 结束的回调
+    const runFn = function () { // 对运行的回调进行二次封装
+        if (opts.isToTime) {
+            run(secondsToTime(seconds)); // 运行时的回调
+        } else {
+            run({day: 0, hours: 0, minutes: 0, seconds: seconds, allSeconds: allSeconds}); // 运行时的回调
+        }
+    };
+    if (Number(seconds) === 0) {
+        if (opts.isHandleRunWhenZero) {
+            runFn(); // 运行时的回调
+        }
+    }
     if (seconds > 0) { // 时间大于0秒
-        const allSeconds = seconds; // 总秒数
-        const run = opts.callback.run; // 运行的回调
-        const over = opts.callback.over; // 结束的回调
-        const runFn = function () { // 对运行的回调进行二次封装
-            if (opts.isToTime) {
-                run(secondsToTime(seconds)); // 运行时的回调
-            } else {
-                run({day: 0, hours: 0, minutes: 0, seconds: seconds, allSeconds: allSeconds}); // 运行时的回调
-            }
-        };
         runFn(); // 运行时的回调
         // 倒计时走你
         const timer = setInterval(function () {
