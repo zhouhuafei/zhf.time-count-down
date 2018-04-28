8f4f19983786f67b9b8175628e82c705f89b13ae
zhouhuafei <1123486116@qq.com>
2018-04-29 06:08:39 +0800

    clear

--- dist/index.js
+++ dist/index.js
@@ -38,18 +38,22 @@ function timeCountDown(json) {
         // 倒计时走你
         var timer = setInterval(function () {
             seconds--;
-            if (seconds === 0) {
-                // 这里如果是小于0，就会存在1秒的误差。这里如果是等于0，则0会展示不出来。
+            /*
+            if (seconds === 0) { // 这里如果是小于0，就会存在1秒的误差。这里如果是等于0，则0会展示不出来。
                 clearInterval(timer); // 清除定时器
-                if (opts.isHandleRunWhenOver) {
-                    // 等于0，则0会展示不出来，为了能让0展示出来一瞬间，这里处理了一下。
+                if (opts.isHandleRunWhenOver) { // 等于0，则0会展示不出来，为了能让0展示出来一瞬间，这里处理了一下。
                     runFn(); // 运行时的回调
                 }
+                over(); // 结束时的回调
+            }
+            */
+            if (seconds < 0) {
+                clearInterval(timer); // 清除定时器
                 over(); // 结束时的回调
             } else {
                 /*
                 * 这里会有一秒的误差，是为了把0秒展示出来停留一秒钟，再过1秒后才结束。
-                * 如果想要非常精确，则需要等于0的时候，先运行次runFn，然后清除定时器，然后结束回调。
+                * 如果想要非常精确，则需要等于0的时候终止定时器，先运行次runFn，然后清除定时器，然后结束回调。
                 * */
                 runFn(); // 运行时的回调
             }
